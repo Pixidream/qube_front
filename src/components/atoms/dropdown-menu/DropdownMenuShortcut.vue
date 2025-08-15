25d3393ef61a0cf4e7d4a0f7db9f56d3b20f3ec6
pixidream <developer@studiopixidream.com>
2025-08-15 23:39:26 +0200

    feat: new dropdown-menu component

--- src/components/atoms/dropdown-menu/DropdownMenuShortcut.vue
+++ src/components/atoms/dropdown-menu/DropdownMenuShortcut.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
-import type { HTMLAttributes } from 'vue'
-import { cn } from '@utils/tailwindMerge'
+import type { HTMLAttributes } from 'vue';
+import { cn } from '@utils/tailwindMerge';
 
-const props = defineProps<{
-  class?: HTMLAttributes["class"]
-}>()
+const props = defineProps<{ class?: HTMLAttributes['class'] }>();
 </script>
 
 <template>
   <span
     data-slot="dropdown-menu-shortcut"
-    :class="cn('text-muted-foreground ml-auto text-xs tracking-widest', props.class)"
+    :class="
+      cn('text-muted-foreground ml-auto text-xs tracking-widest', props.class)
+    "
   >
     <slot />
   </span>
