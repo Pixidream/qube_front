6c75e24d52a651f498b8bfc653851ea5e541a860
pixidream <developer@studiopixidream.com>
2025-08-21 16:49:32 +0200

    chore: disable eslint warnings in generated code

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,7 +3,7 @@
 declare module '*.vue' {
   import type { DefineComponent } from 'vue';
   /* eslint-disable @typescript-eslint/no-empty-object-type  */
-  /* eslint-disable @typescript-eslint/no-explicit-any */
+   
   const component: DefineComponent<{}, {}, any>;
   export default component;
 }
