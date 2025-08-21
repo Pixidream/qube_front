02e0f1b826512e5bc508da6ad04dbfa342026153
pixidream <developer@studiopixidream.com>
2025-08-21 16:50:25 +0200

    chore: file format issue

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,7 +3,6 @@
 declare module '*.vue' {
   import type { DefineComponent } from 'vue';
   /* eslint-disable @typescript-eslint/no-empty-object-type  */
-   
   const component: DefineComponent<{}, {}, any>;
   export default component;
 }
