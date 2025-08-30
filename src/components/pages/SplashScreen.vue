ed1b60af9519dea7911d4892bfb361642a1918dd
pixidream <developer@studiopixidream.com>
2025-08-30 22:37:34 +0200

    fix: splashscreen watches appMachine state to redirect to login if necessary

--- src/components/pages/SplashScreen.vue
+++ src/components/pages/SplashScreen.vue
@@ -1,13 +1,23 @@
 <script setup lang="ts">
 import { useAppMachine } from '@/machines/app.machine';
 import { useAppStore } from '@/stores/app.stores';
+import { useAuthStore } from '@/stores/auth.stores';
 import AppLogo from '@assets/images/logo.png';
 import { onMounted } from 'vue';
 import { useI18n } from 'vue-i18n';
+import { useRouter } from 'vue-router';
 
 const { t } = useI18n();
 const appStore = useAppStore();
 const appMachine = useAppMachine();
+const authStore = useAuthStore();
+const router = useRouter();
+
+appMachine.actor.subscribe((snapshot) => {
+  if (!authStore.isAuthenticated && snapshot.matches('loaded')) {
+    router.push({ name: 'login' });
+  }
+});
 
 const initApp = async () => {
   const success = await appStore.initApp();
@@ -24,9 +34,7 @@ onMounted(() => {
   <div
     class="safe-height w-dvw flex flex-col justify-center items-center bg-background"
   >
-    <!-- Main content -->
     <div class="flex flex-col items-center space-y-6">
-      <!-- Logo -->
       <div class="animate-fade-in">
         <img
           :src="AppLogo"
@@ -34,15 +42,11 @@ onMounted(() => {
           class="w-20 h-20 object-contain"
         />
       </div>
-
-      <!-- App name -->
       <div class="text-center animate-fade-in">
         <h1 class="text-2xl font-semibold text-foreground tracking-tight">
           {{ t('app.name') }}
         </h1>
       </div>
-
-      <!-- Loading indicator -->
       <div class="flex space-x-1">
         <div
           class="w-2 h-2 bg-muted-foreground rounded-full animate-pulse"
