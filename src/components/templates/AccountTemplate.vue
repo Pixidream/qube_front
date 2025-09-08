7191a20365e7e063361cfd00a105a4a3176948c4
pixidream <developer@studiopixidream.com>
2025-09-08 20:52:45 +0200

    feat: update account template to show tabs to switch routes

--- src/components/templates/AccountTemplate.vue
+++ src/components/templates/AccountTemplate.vue
@@ -1,30 +1,51 @@
 <script setup lang="ts">
-// import { useI18n } from 'vue-i18n';
-// import { useRoute, useRouter } from 'vue-router';
+import { useI18n } from 'vue-i18n';
 import ProfileCard from '@components/organisms/account/ProfileCard.vue';
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from '@components/atoms/tabs';
+import accountRoutes from '@router/routes/account';
+import { type RouteMeta } from '@core/types/router';
+import { type RouteRecordNameGeneric, useRouter } from 'vue-router';
+import { computed } from 'vue';
 
-// const route = useRoute();
-// const router = useRouter();
-// const { t } = useI18n();
-// const routes: {
-//   name: string;
-//   display: string;
-//   icon: string;
-// }[] = [
-//   {
-//     name: 'profile',
-//     display: t('navigation.profile'),
-//     icon: 'lucide:user',
-//   },
-//   {
-//     name: 'security',
-//     display: t('navigation.security'),
-//     icon: 'lucide:shield',
-//   },
-// ];
+const { t } = useI18n();
+const router = useRouter();
+
+const currentTab = computed(() => router.currentRoute.value.name);
+
+const handleTrigger = (routeName: RouteRecordNameGeneric) => {
+  router.push({ name: routeName });
+};
 </script>
 <template>
-  <div class="container mx-auto space-y-6 px-4 py-10 md:max-w-4/5">
+  <div class="container mx-auto space-y-6 px-4 py-10">
     <ProfileCard />
+    <div>
+      <Tabs :model-value="currentTab as string" class="w-full">
+        <TabsList class="grid w-full grid-cols-2">
+          <TabsTrigger
+            v-for="accountRoute of accountRoutes[0].children"
+            :key="accountRoute.name"
+            :value="accountRoute.name as string"
+            @click="handleTrigger(accountRoute.name)"
+          >
+            {{
+              t((accountRoute.meta as RouteMeta | undefined)?.displayName ?? '')
+            }}
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent
+          v-for="accountRoute of accountRoutes[0].children"
+          :key="accountRoute.name"
+          :value="accountRoute.name as string"
+        >
+          <RouterView :key="accountRoute.name" />
+        </TabsContent>
+      </Tabs>
+    </div>
   </div>
 </template>
