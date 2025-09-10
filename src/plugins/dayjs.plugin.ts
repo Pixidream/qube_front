2633ab21ec3f679e135ec0aaed4d51e94ddf7145
pixidream <developer@studiopixidream.com>
2025-09-10 18:09:37 +0200

    feat: add relativeTime plugin to dayjs

--- src/plugins/dayjs.plugin.ts
+++ src/plugins/dayjs.plugin.ts
@@ -2,9 +2,11 @@ import dayjs from 'dayjs';
 import duration from 'dayjs/plugin/duration';
 import utc from 'dayjs/plugin/utc';
 import timezone from 'dayjs/plugin/timezone';
+import relativeTime from 'dayjs/plugin/relativeTime';
 
 dayjs.extend(duration);
 dayjs.extend(utc);
 dayjs.extend(timezone);
+dayjs.extend(relativeTime);
 
 export default dayjs;
