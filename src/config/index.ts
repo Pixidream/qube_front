c651cd4d6bce6304f37b0219a874272db7c0a57c
pixidream <developer@studiopixidream.com>
2025-10-21 15:00:21 +0200

    fix: moving config from hardcoded to env based

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,19 +3,10 @@ import { logger } from '@/utils/logger';
 const configLogger = logger.child({ utility: 'appConfig' });
 
 export const appConfig = {
-  development: {
-    api: { baseUrl: 'https://192.168.1.47:3000/api/v1' },
-    initialization: {
-      maxRetries: 3,
-      retryDelayMs: 10,
-    },
-  },
-  production: {
-    api: { baseUrl: 'https://192.168.1.47:3000/api/v1' },
-    initialization: {
-      maxRetries: 2,
-      retryDelayMs: 2000,
-    },
+  api: { baseUrl: import.meta.env.VITE_API_BASE_URL },
+  initialization: {
+    maxRetries: import.meta.env.VITE_INIT_MAX_RETRIES,
+    retryDelayMs: import.meta.env.VITE_INIT_RETRY_DELAY_MS,
   },
 };
 
@@ -25,9 +16,9 @@ configLogger.info('Loading application configuration', {
   action: 'config_load',
   environment,
   isDev: import.meta.env.DEV,
-  apiBaseUrl: appConfig[environment].api.baseUrl,
-  maxRetries: appConfig[environment].initialization.maxRetries,
-  retryDelayMs: appConfig[environment].initialization.retryDelayMs,
+  apiBaseUrl: appConfig.api.baseUrl,
+  maxRetries: appConfig.initialization.maxRetries,
+  retryDelayMs: appConfig.initialization.retryDelayMs,
 });
 
-export const APP_CONFIG = appConfig[environment];
+export const APP_CONFIG = appConfig;
